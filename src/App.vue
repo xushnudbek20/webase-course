67ecbd5e283ad3fdbae81661aa9a3521a8497aa5
Xushnud <xushnud.king@gmail.com>
2022-12-03 12:42:06 +0500

    Lasts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Webase Market
+    </h1>
+    <div class="cards">
+      <TestComponents 
+        v-for="(product, index) in products" 
+        :key="index"
+        :name="product.name"
+        :price="product.price"
+        :sale="product.sale"
+        @count="productCount($event, index)"
+       />
+    </div>
+     <div v-if="products_card.length" class="product-card">
+      <div v-for="(product,index) in products_card" :key="index" class="cardd">
+          <p>Mahsulot: {{product.name}}</p>
+          <p>Soni: {{product.count}}</p>
+          <p>Narxi: {{product.price*product.count}}</p>
+          <hr>
+      </div>
+      <b>Umumiy summa: </b>{{getTotalSum()}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponents from "./components/test-comp.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestComponents
+  },
+  data(){
+    return {
+      products:[
+        { name:"Iphone 14 pro Max", price:1500, count: 0, sale:true},
+        { name:"Iphone 13 pro Max", price:1300, count: 0, sale:true},
+        { name:"Iphone 12 pro Max", price:1200, count: 0, sale:true},
+        { name:"Tv Smart", price:200, count: 0, sale:false},
+      ],
+      products_card:[],
+      totalSum: 0
+    }
+  },
+  methods:{
+    productCount(count, index){
+      this.products[index].count = count
+      if (!this.products_card.find(el=>el.name==this.products[index].name)) {
+        this.products_card.push(this.products[index])
+      }
+    },
+    getTotalSum(){
+      let totalSum = 0
+      this.products_card.forEach(el => {
+        totalSum += el.price*el.count
+      })
+      return totalSum
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('./assets/styles.css');
 </style>
---
+++ src/components/test-comp.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <div class="info">
+      <p class="name">{{ name }}</p>
+      <span class="cost">Cost: {{price}} $</span> <br>
+      <b v-if="sale" class="text-danger">Skidka</b>
+    </div>
+    <div class="btn">
+      <button v-if="count_product==0" @click="addToCard">Add to card</button>
+    </div>
+    <div v-if="count_product" class="calc-prod">
+      <button @click="calcCount(false)">-</button>
+      {{count_product}}
+      <button @click="calcCount(true)">+</button>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'JustComponent',
+  props: {
+    name:{
+      type: String,
+      default: 'Name'
+    },
+    price:{
+      type: Number,
+      default:1000
+    },
+    sale:{
+      type: Boolean,
+      default: false
+    }
+  },
+  data(){
+    return {
+      count_product: 0
+    }
+  },
+  computed:{
+  },
+  methods:{
+    addToCard(){
+      this.count_product = 1
+      this.$emit('count',  this.count_product)
+    },
+    increment(){
+      },
+    calcCount(isInc){
+      if (isInc) {
+        this.count_product = this.count_product + 1
+      } else if(this.count_product>0){
+        this.count_product = this.count_product - 1
+      }
+      this.$emit('count',  this.count_product)
+
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
